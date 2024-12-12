ae093fe2b498ba49c042a04a87091b6775bcc40e
shashant <shashantyashu2004@gmail.com>
2024-12-12 20:29:10 +0530

    maked it responsive

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
  
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+ const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
-const dbUrl = process.env.ATLASDB_URL;
+// const dbUrl = process.env.ATLASDB_URL;
 
 main().then(() => {
     console.log("connected to DB");
@@ -33,7 +33,7 @@ main().then(() => {
 });
 
 async function main() {
-    await mongoose.connect(dbUrl);
+    await mongoose.connect(MONGO_URL);
 };
 
 app.set("view engine", "ejs");
@@ -44,7 +44,7 @@ app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 const store = MongoStore.create({
-    mongoUrl: dbUrl,
+    mongoUrl: MONGO_URL, // dbUrl
     crypto: {
         secret: process.env.SECRET,
     },
